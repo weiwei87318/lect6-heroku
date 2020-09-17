d9ab4bae02037ee636adb62f69496e9d96790012
EC2 Default User <ec2-user@ip-172-31-1-225.ec2.internal>
2020-09-17 18:01:22 +0000

    Add list of Taylor Swift's tweets to HTML

---
+++ user_tweets.py
@@ -0,0 +1,62 @@
+import tweepy
+import os
+import datetime
+import flask
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+app = flask.Flask(__name__)
+
+dotenv_path = join(dirname(__file__), 'tweepy.env')
+load_dotenv(dotenv_path)
+
+
+consumer_key = os.environ['KEY']
+consumer_secret = os.environ['KEY_SECRET']
+access_token = os.environ['TOKEN']
+access_token_secret = os.environ['TOKEN_SECRET']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+account = "taylorswift13"
+
+@app.route('/')
+
+def index():
+
+    user = api.get_user(account)
+    screen_name = user.screen_name
+    user_name = user.name
+    description = user.description
+    
+    user_tweets = api.user_timeline(account)
+    end_date = datetime.datetime.now() - datetime.timedelta(days=30)
+    
+    tweet_list = []
+    tweet_time = []
+    
+    # get all tweets in the last 30 days
+    for tweet in user_tweets:
+      
+        if tweet.created_at < end_date: 
+            break
+        
+        tweet_list.append(tweet.text)
+        tweet_time.append(tweet.created_at)
+       
+    return flask.render_template(
+        "index.html",
+        screen_name = screen_name,
+        user_name = user_name,
+        description = description,
+        tweet_list = tweet_list,
+        tweet_time = tweet_time,
+        list_len = len(tweet_list),
+        time_len = len(tweet_time))
+    
+app.run(
+    port = int(os.getenv("PORT", 8080)),   
+    host = os.getenv("IP" "0.0.0.0")
+)        
